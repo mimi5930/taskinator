a2b341538993706f7ec5e1e480bb2543160f805d
Mike <mikej.miller440@gmail.com>
2021-12-06 13:35:11 -0600

    added form functionality in js"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
-var buttonE1 = document.querySelector("#save-task");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
+var createTaskHandler = function(event) { 
+  event.preventDefault(); 
 
-var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-};
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+  listItemEl.textContent = "This is a new task."; 
+  tasksToDoEl.appendChild(listItemEl); 
+}; 
 
-buttonE1.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
